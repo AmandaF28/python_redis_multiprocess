b2d18b8b3254b3546f92afbab7c9aa04630e25bc
AmandaF28 <62296457+AmandaF28@users.noreply.github.com>
2020-03-19 17:31:37 -0300

    Update redis_modules.py

--- redis_modules.py
+++ redis_modules.py
@@ -1,5 +1,17 @@
-from time import sleep
+#from time import sleep
+import random
 
-def print_as_function(argument):
-  sleep(2)
-  return argument
+def cria_matriz(linhas, colunas):
+    A = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha = linha + [random.randint(1, 10)]
+        A = A + [linha]
+    return A
+  
+def multiplica_linha_coluna(matrizA, matrizB, i, j):
+    valor = 0
+    for k in range(len(matrizB)):
+        valor = valor + matrizA[k] * matrizB[k]
+    return i, j, valor
